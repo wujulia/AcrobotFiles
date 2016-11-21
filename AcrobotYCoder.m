5f4d23a7b64fffd0d6e8010dcd2119c14aed72cb
mposa <mposa@mit.edu>
2016-11-21 18:55:42 -0500

    Moved sign flips into coders-fix for the last commit. Added some differencing/dynamics code to log parsing script.

--- AcrobotYCoder.m
+++ AcrobotYCoder.m
@@ -9,7 +9,7 @@ classdef AcrobotYCoder < LCMCoder
           conv = 1/5215;
           ticks_1 = 32768; %2^15;
           ticks_2 = 10000;
-          conv = [2*pi/ticks_1; 2*pi/ticks_2; 0; 0];
+          conv = [-2*pi/ticks_1; 2*pi/ticks_2; 0; 0];
           y = conv.*y;
           %warning('thetadot not implemented yet');
           t = msg.timestamp/1000;
@@ -18,7 +18,7 @@ classdef AcrobotYCoder < LCMCoder
       function msg = encode(obj,t,y)
           msg = acrobot_types.lcmt_acrobot_y();
           msg.timestamp = t*1000;
-          msg.theta1 = y(1);
+          msg.theta1 = -y(1); % sign flip to match ecoder
           msg.theta2 = y(2);
           warning('tau as output is not implemented yet');
           msg.tau = 0;
--- parseAcrobotLog.m
+++ parseAcrobotLog.m
@@ -8,3 +8,25 @@ data = readLog(filename,channels,coders);
 
 %%
 plant = AcrobotPlantSmooth;
+
+t = data{2}.t;
+x = data{2}.data;
+u = data{3}.data;
+
+u = interp1(data{3}.t,u,t);
+
+t = t-t(1);
+
+
+xdot = x*0;
+for i=1:length(t),
+  xdot(:,i) = plant.dynamics(t(i),x(:,i),u(i));
+end
+
+q = x(1:2,:);
+v = x(3:4,:);
+v_diff = [diff(q(1,:))./diff(t);diff(q(2,:))./diff(t)];
+v_diff = [zeros(2,1) v_diff];
+
+vdot_diff = [diff(v(1,:))./diff(t);diff(v(2,:))./diff(t)];
+vdot_diff = [zeros(2,1) vdot_diff];
