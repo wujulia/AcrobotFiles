5581c89153caa0a2090dc11492b382617eaf05c1
mposa <mposa@mit.edu>
2016-11-22 13:33:44 -0500

    Made AcrobotPlantSmooth a Manipulator. Added manual gradient calculations.

--- AcrobotPlantSmooth.m
+++ AcrobotPlantSmooth.m
@@ -1,7 +1,7 @@
-classdef AcrobotPlantSmooth < DrakeSystem
+classdef AcrobotPlantSmooth < Manipulator
   % Defines the dynamics for the real acrobot
   % Updated on Jan 20, 2014.
-  
+  %#ok<*PROPLC>
   properties
     
     
@@ -35,55 +35,47 @@ classdef AcrobotPlantSmooth < DrakeSystem
   
   methods
     function obj = AcrobotPlantSmooth()
-      obj = obj@DrakeSystem(4,0,1,4,0,1);
+      obj = obj@Manipulator(2,1,2);
       obj = obj.setOutputFrame(obj.getStateFrame);
-    end
+    end    
     
-    function [xdot,df] = dynamics(obj,t,x,u)
-      
-      if nargout > 1
-        [xdot,df] = geval(@obj.dynamicsFun,t,x,u);
-      else
-        xdot = obj.dynamicsFun(t,x,u);
-      end
-    end
-    
-    function xdot = dynamicsFun(obj,t,x,u)
-      q = x(1:2);
-      qd = x(3:4);
-      
-      xdot = x*0;
-      
-      % First derivatives
-      xdot(1) = x(3);
-      xdot(2) = x(4);
-      
+    function [H,C,B,dH,dC,dB] = manipulatorDynamics(obj,q,v)      
       % Compute manipulator dynamics
       % keep it readable:
       m1=obj.m1; m2=obj.m2; l1=obj.l1; g=obj.g; lc1=obj.lc1; lc2=obj.lc2; I1=obj.I1; I2=obj.I2; b1=obj.b1; b2=obj.b2;
       m2l1lc2 = m2*l1*lc2;  % occurs often!
       
-      c = cos(q(1:2,:));  s = sin(q(1:2,:));  s12 = sin(q(1,:)+q(2,:));
+      c = cos(q(1:2,:));  s = sin(q(1:2,:));  s12 = sin(q(1,:)+q(2,:)); c12 = cos(q(1,:)+q(2,:));
       
       h12 = I2 + m2l1lc2*c(2);
       H = [ I1 + I2 + m2*l1^2 + 2*m2l1lc2*c(2), h12; h12, I2 ];
       
-      C = [ -2*m2l1lc2*s(2)*qd(2), -m2l1lc2*s(2)*qd(2); m2l1lc2*s(2)*qd(1), 0 ];
+      
+      C1 = [ -2*m2l1lc2*s(2)*v(2), -m2l1lc2*s(2)*v(2); m2l1lc2*s(2)*v(1), 0 ];
+      
+
+      
       G = g*[ m1*lc1*s(1) + m2*(l1*s(1)+lc2*s12); m2*lc2*s12 ];
       
+      
+      
       % accumate total C and add a damping term:
-      C = C*qd + G + [b1;b2].*qd;
+      b = diag([b1;b2]);
+      C = C1*v + G + b*v;
       
       B = [0; 1];
-      tau=B*u;
       
-      % Compute second derivatives
-      qdd = H\(tau - C);
-      xdot(3) = qdd(1);
-      xdot(4) = qdd(2);
-      
-      if (nargout>1)
-        %             [df,d2f,d3f]= dynamicsGradients(obj,t,x,u,nargout-1);
+      if nargout > 1
+        dHdq = [zeros(4,1) [-2*m2l1lc2*s(2); -m2l1lc2*s(2); -m2l1lc2*s(2);0]];
+        dH = [dHdq zeros(4,2)];
+        
+        dG = g*[[m1*lc1*c(1) + m2*(l1*c(1)+lc2*c12), m2*lc2*c12; m2*lc2*c12, m2*lc2*c12], zeros(2,2)];
+        dC1dq2 = [-2*m2l1lc2*c(2)*v(2); m2l1lc2*c(2)*v(1);-m2l1lc2*c(2)*v(2); 0];
+        dC1dv = [0 -2*m2l1lc2*s(2); m2l1lc2*s(2) 0; 0 -m2l1lc2*s(2); 0 0];
+        dC1 = [zeros(4,1), dC1dq2, dC1dv];
+        dC = matGradMult(dC1,v) + [zeros(2,2) C1+b] + dG;
+        
+        dB = zeros(2,4);
       end
     end
     
